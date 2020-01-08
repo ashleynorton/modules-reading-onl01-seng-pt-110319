958bad57c5d092556d69266ad83d028f5984e09f
Ashley Norton <nortonashleym@gmail.com>
2020-01-08 02:45:24 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,4 +1,14 @@
-class Dancer
-  extend
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
+
+class Dancer 
+  include Dance 
+  extend MetaDancing
+  
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
   
 end 
